68a66e09480d961360825475e0b257f858db88c3
Jyoti Narang <jyotinarang@lawn-128-61-7-47.lawn.gatech.edu>
2017-11-24 14:49:05 -0500

    changes to the humquery function

--- humquery.py
+++ humquery.py
@@ -9,64 +9,53 @@ print('Import successful')
 
 '''This function will '''
 def pre_process(file):
-	data = pd.read_excel(file)
-	annotation = data['Annotation']
-	new_array = []
-	for i in range(len(annotation)):
-		if(annotation[i]<=0):
-			new_array.append(0)
-		else:
-			new_array.append(annotation[i])
-	return new_array
+    data = pd.read_excel(file)
+    annotation = data['Annotation']
+    new_array = []
+    for i in range(len(annotation)):
+        if(annotation[i]<=0):
+            new_array.append(0)
+        else:
+            new_array.append(annotation[i])
+    return new_array
 
 #Convert the pre-proccessed data to MIDI
 def convert_to_midi(input_array):
-	array_to_midi = []
-	for i in range(len(input_array)):
-		if(input_array[i] == 0):
-			array_to_midi.append(input_array[i])
-		else:
-			midi_value = 69 + 12*math.log(input_array[i]/440,2)
-			array_to_midi.append(midi_value)
-	return array_to_midi
+    array_to_midi = []
+    for i in range(len(input_array)):
+        if(input_array[i] == 0):
+            array_to_midi.append(input_array[i])
+        else:
+            midi_value = 69 + 12*math.log(input_array[i]/440,2)
+            array_to_midi.append(midi_value)
+    return array_to_midi
 
-def getCostMatrices(reference, query, compare):  
-    '''
-    This function takes in the reference and the query objects and returns a tuple 
-    of two cost matrices where the first(costMatrix) considers the distance between the two objects in the cost
-    while the other(costMatrixLCS) does not. It also returns the trace back matrice which tells us the path back
-    for the best match.
-    The distance is defined based on 'compare function'
-    :param reference: list of the object considered as the target list
-    :param query: list of the object considered as the query list
-    :param compare: a funcion which returns a truple (isEqual, distance) where
-        'isEqual' says if the two are close enough
-        'distance' is the distance between them
-    '''
-    # This is the list to be seached into; normally larger than the reference
-    n = len(reference)
-
-    #This is th list of objects to be searched for normally small compare to reference
-    m = len(query)
-
-    #Initializing the matrix with size one greater than the query and refernce sizes to fill up a dummy position
-    costMatrix = np.zeros((n + 1, m + 1))
-    costMatrixLCS = np.zeros((n + 1, m + 1))
-
-    for i in range(n):
-        for j in range(m):
-            isEqual, dist = compare.getDist(reference[i], query[j])
-            if isEqual == True:
-                costMatrix[i+1][j+1] = costMatrix[i][j] + (1 - dist)
-                costMatrixLCS[i+1][j+1] = costMatrixLCS[i][j] + 1
+#Store the midi values as a relative sequence of numbers
+def midi_to_numbers(input_array):
+    midi_relative_numbers = [0]
+    #import pdb;pdb.set_trace()
+    for i in range(1,len(input_array)):
+        if(input_array[i]>input_array[i-1]):
+            if(abs(input_array[i]-input_array[i-1])>1):
+                sequence = midi_relative_numbers[i-1] + 1
+                midi_relative_numbers.append(sequence)
             else:
-                costMatrix[i+1][j+1] = max(costMatrix[i][j+1], costMatrix[i+1][j])
-                costMatrixLCS[i+1][j+1] = max(costMatrixLCS[i][j+1], costMatrixLCS[i+1][j])
+                midi_relative_numbers.append(midi_relative_numbers[i-1])
+        elif(input_array[i]<input_array[i-1]):
+            if(abs(input_array[i]-input_array[i-1])>1):
+                sequence = midi_relative_numbers[i-1]-1
+                midi_relative_numbers.append(sequence)
+            else:
+                midi_relative_numbers.append(midi_relative_numbers[i-1])
+        else:
+            midi_relative_numbers.append(midi_relative_numbers[i-1])
+    return midi_relative_numbers
 
-    return (costMatrix, costMatrixLCS)
 
-def rlcs(query, query_audio):
 
+
+#This function will convert the input 
+#def convert_segments(input_array)
 
 if __name__ == '__main__':
     file_audio = r'q7_freq.xlsx'
@@ -75,6 +64,9 @@ if __name__ == '__main__':
     file_query = r'q7_query.xlsx'
     pre_processed_query = pre_process(file_query)
     midi_values_query = convert_to_midi(pre_processed_query)
+    midi_relative_numbers = midi_to_numbers(midi_values_query)
+    print(midi_values_query)
+    print(midi_relative_numbers)
 
 
 
